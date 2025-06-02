6d202e51d7a12a41f036c6a7181b921cafd55c25
albim72 <marcin.albiniak72@gmail.com>
2025-06-02 11:18:15 +0200

    Create test.py

---
+++ DZIEN_1/test.py
@@ -0,0 +1,32 @@
+a:int = 600
+print(a)
+print(type(a))
+
+a = "ładna pogoda"
+print(a)
+print(type(a))
+
+#komentarz
+"""
+komentarz wieloliniowy
+dokumentacyjny
+"""
+
+s = """
+abc
+53242353
+----------
+d=45
+"""
+
+print(s)
+
+#lista
+l = [1,2,3,4,5,64,8,35,9,22,6,1,2,1,2]
+print(l)
+print(l[4])
+print(l[4:8])
+print(l[4:])
+print(l[:8])
+print(l[::-1])
+print(l[7:1:-2])
